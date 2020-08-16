fd24f0b4bef809d1acf40d9613a180eb05f7849b
pace11 <ryanjoker87@gmail.com>
2020-08-16 16:56:14 +0700

    feat: improve homepage web

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
-import theme from '../../common/theme'
+import Layout from '../../common/components/Layout'
 
-const WrapperHome = styled.div`
-  width: 100%;
-  height: 100px;
-  background-color: ${theme.colors.pink};
-`
-
-function StyledHome() {
-  return (
-    <WrapperHome>
-      <p>Home</p>
-    </WrapperHome>
-  )
+export default function Home() {
+  return <Layout />
 }
-
-export default StyledHome
